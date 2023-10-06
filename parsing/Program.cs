6924ba5d4b1a8cf2b958e0c7295e16e6321bbe01
vladislaveeeeee <roshkovlad2007@gmail.com>
2023-10-06 11:04:39 +0300

    Add project files.

---
+++ parsing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string response = @"0;1375081017007083525; @telegram @apple @GooglePlay;875607916480311296;;;;;;";
+
+        string[] lines = response.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            if (parts.Length >= 4)
+            {
+                string authorId = parts[3];
+                Console.WriteLine("Author ID: " + authorId);
+            }
+        }
+    }
+}
